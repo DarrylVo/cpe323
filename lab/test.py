6a8906fe0b561c3ce9c82477debd1f04095c17b4
darryl <darryl@darryl-Satellite-S55D-A>
2016-01-12 17:48:37 -0800

    stuff

--- lab/test.py
+++ lab/test.py
@@ -2,25 +2,27 @@ import base64
 import binascii
 from operator import xor
 def asciitohex(ascii):
-    return binascii.b2a_hex(ascii)
+    return base64.b16encode(ascii)
 def hextoascii(hex):
-    return binascii.a2b_hex(hex)
-
-def hextobase(hex):
-    return binascii.b2a_base64(hex)
+    return base64.b16decode(hex, casefold=True)
 def basetohex(base):
-    return binascii.a2b_base64(base)
-def strxor(str1,key): #assumes hex encoded
-    a=int(str1, 16)
-    b=int(key,16)
-    if a.len()> b.len():
+    base = base64.b64decode(base)
+    return base64.b16encode(base)
+def hextobase(hex):
+    hex = base64.b16decode(hex,casefold=True)
+    return base64.b64decode(hex)
+def strxor(str1, key):
+    z= 1;
+    for a in str1:
         
-
     
     
 f = open('test.txt','r')
+
 for line in f:
     print (line)
-    print (strxor(line,line))
-
+    a=hextoascii(line[:-1])
+    strxor(a,a)
+    
+    #a = line.decode("hex")
 
